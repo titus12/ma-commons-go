0ba22a160a2246c4ea619e2867f80bb144908706
titus12 <58381026@qq.com>
2020-07-05 15:14:48 +0800

    services update

--- services/server.go
+++ services/server.go
@@ -3,25 +3,25 @@ package services
 import (
 	"context"
 	log "github.com/sirupsen/logrus"
+	gp "github.com/titus12/ma-commons-go/services/pb-grpc"
 	"github.com/titus12/ma-commons-go/utils"
 	"google.golang.org/grpc"
 	"math"
 	"net"
-	gp "github.com/titus12/ma-commons-go/services/pb-grpc
 )
 
 type server struct{}
 
 func (s *server) Ready(context.Context, *gp.Node) (*gp.Result, error) {
 	defer utils.PrintPanicStack()
-	return nil,nil
+	return nil, nil
 }
 
-func startServer(listen string)(*server,error) {
+func startServer(listen string) (*server, error) {
 	// 监听
 	lis, err := net.Listen("tcp", listen)
 	if err != nil {
-		return nil ,err
+		return nil, err
 	}
 	log.Info("listening on ", lis.Addr())
 
@@ -30,5 +30,5 @@ func startServer(listen string)(*server,error) {
 	ins := &server{}
 	gp.RegisterServiceServer(s, ins)
 	log.Infof("开始监听service %s", listen)
-	return ins,nil
+	return ins, nil
 }
