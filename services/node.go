219637c167fa52b8db4135237496061512aa7454
huwei <huwei@gg.com>
2020-07-08 16:26:42 +0800

    添加注解，发现master有更新，先去合master

--- services/node.go
+++ services/node.go
@@ -2,10 +2,11 @@ package services
 
 import "google.golang.org/grpc"
 
+// 节点状态
 const (
-	ServiceStatusNone = iota
-	ServiceStatusPending
-	ServiceStatusRunning
+	ServiceStatusNone    = iota // 节点起动中，还未加入到集群，暂时不可提供服务，可以认为服务就是不存在
+	ServiceStatusPending        // 节点已加入到服务，但是目前还是在等其他节点完成操作（比如数据牵移）
+	ServiceStatusRunning        // 节点处于正常
 	ServiceStatusStopping
 )
 
