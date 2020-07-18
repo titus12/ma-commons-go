aa64ef7f3b480f9ead12dfad4751cb2852f00f48
titus12 <58381026@qq.com>
2020-07-18 11:03:42 +0800

    protocol code rename

--- net/pack.go
+++ net/pack.go
@@ -1,7 +1,7 @@
 package net
 
 import (
-	"log"
+	log "github.com/sirupsen/logrus"
 	"reflect"
 )
 
--- net/protocol_code.go
+++ net/protocol_code.go
@@ -7,8 +7,8 @@ import (
 )
 
 type ProtocolNumber struct {
-	names map[string]int32
-	ids   map[int32]string
+	names map[string]int16
+	ids   map[int16]string
 	mu    sync.RWMutex
 }
 
@@ -25,29 +25,29 @@ func (p *ProtocolNumber) init() {
 func (p *ProtocolNumber) load(msg descriptor.Message) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	names := make(map[string]int32)
-	ids := make(map[int32]string)
+	names := make(map[string]int16)
+	ids := make(map[int16]string)
 	_, md := descriptor.ForMessage(msg)
 	for i := 0; i < len(md.Field); i++ {
 		v := md.GetField()[i]
 		if _, ok := names[v.GetName()]; ok {
 			return fmt.Errorf("parse protocol number, repeated protocol id %v..", v.GetName())
 		}
-		names[v.GetName()] = v.GetNumber()
-		ids[v.GetNumber()] = v.GetName()
+		names[v.GetName()] = int16(v.GetNumber())
+		ids[int16(v.GetNumber())] = v.GetName()
 	}
 	p.names = names
 	p.ids = ids
 	return nil
 }
 
-func (p *ProtocolNumber) getId(name string) int32 {
+func (p *ProtocolNumber) getId(name string) int16 {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 	return p.names[name]
 }
 
-func (p *ProtocolNumber) getName(id int32) string {
+func (p *ProtocolNumber) getName(id int16) string {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 	return p.ids[id]
@@ -60,10 +60,10 @@ func LoadProtocol(msg descriptor.Message) error {
 	return protocolNumber.load(msg)
 }
 
-func GetProtocolId(name string) int32 {
+func GetProtocolId(name string) int16 {
 	return protocolNumber.getId(name)
 }
 
-func GetProtocolName(id int32) string {
+func GetProtocolName(id int16) string {
 	return protocolNumber.getName(id)
 }
